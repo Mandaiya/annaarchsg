9f2f4b8e1a4db6a1a99e6a7b8160b4940de50ee3
natha <108113193+Mandaiya@users.noreply.github.com>
2024-08-03 04:10:36 +0530

    Update thumbnails.py

--- ArchMusic/utils/thumbnails.py
+++ ArchMusic/utils/thumbnails.py
@@ -225,9 +225,9 @@ async def gen_qthumb(videoid):
             #font3 = ImageFont.truetype('assets/font2.ttf', 40)
             #font4 = ImageFont.truetype('assets/font2.ttf', 35)
 
-            image4 = ImageDraw.Draw(image2)
-            image4.text((960, 10), f"{MUSIC_BOT_NAME}", fill="white", font=font1, align="left")
-            image4.text((670, 150), "ADDED TO QUEUE", fill="white", font=font2, stroke_width=2, stroke_fill="red", align="left")
+            #image4 = ImageDraw.Draw(image2)
+            #image4.text((960, 10), f"{MUSIC_BOT_NAME}", fill="white", font=font1, align="left")
+            #image4.text((670, 150), "ADDED TO QUEUE", fill="white", font=font2, stroke_width=2, stroke_fill="red", align="left")
 
             title1 = truncate(title)
             image4.text((670, 300), text=title1[0], fill="white", stroke_width=1, stroke_fill="white", font=font3, align="left")
