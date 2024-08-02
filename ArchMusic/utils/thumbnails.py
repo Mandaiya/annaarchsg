2af319f64ad323be3b13312500662da7f3290812
natha <108113193+Mandaiya@users.noreply.github.com>
2024-08-02 23:29:59 +0530

    Update thumbnails.py

--- ArchMusic/utils/thumbnails.py
+++ ArchMusic/utils/thumbnails.py
@@ -82,9 +82,9 @@ async def gen_thumb(videoid):
             youtube = Image.open(f"cache/thumb{videoid}.jpg")
             image1 = changeImageSize(1280, 720, youtube)
             image2 = image1.convert("RGBA")
-            background = image2.filter(filter=ImageFilter.BoxBlur(20))
+            background = image2.filter(filter=ImageFilter.BoxBlur(15))
             enhancer = ImageEnhance.Brightness(background)
-            background = enhancer.enhance(0.7)
+            background = enhancer.enhance(1.0)
             image2 = background
 
             #circle = Image.open("assets/circle.png")
@@ -121,8 +121,8 @@ async def gen_thumb(videoid):
             font4 = ImageFont.truetype('assets/font2.ttf', 35)
 
             image4 = ImageDraw.Draw(image2)
-            image4.text((960, 10), f"{MUSIC_BOT_NAME}", fill="white", font=font1, align="left")
-            image4.text((100, 150), "NOW PLAYING", fill="white", font=font2, stroke_width=2, stroke_fill="white", align="left")
+            image4.text((300, 20), f"{MUSIC_BOT_NAME}", fill="white", font=font1, align="left")
+            image4.text((200, 150), "NOW PLAYING", fill="Yellow", font=font2, stroke_width=4, stroke_fill="white", align="left")
 
             title1 = truncate(title)
             image4.text((100, 300), text=title1[0], fill="white", stroke_width=1, stroke_fill="white", font=font3, align="left")
