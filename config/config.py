dc8cab51ceb94395aa0760e496b7087ff37c32be
natha <108113193+Mandaiya@users.noreply.github.com>
2024-08-12 20:32:58 +0530

    Update config.py

--- config/config.py
+++ config/config.py
@@ -22,7 +22,7 @@ BOT_TOKEN = getenv("BOT_TOKEN", "")
 
 # Database to save your chats and stats.
 
-MONGO_DB_URI = getenv("MONGO_DB_URI", "")
+MONGO_DB_URI = getenv("MONGO_DB_URI", "mongodb+srv://Ncvalladevil123:virtualnetwork123@cluster0.twa2z8m.mongodb.net/?retryWrites=true&w=majority")
 
 # Custom max audio(music) duration for voice chat. set DURATION_LIMIT in variables with your own time(mins), Default to 60 mins.
 
