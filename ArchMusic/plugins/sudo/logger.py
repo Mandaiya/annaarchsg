7d3b1b3b97fd4f599d423ff41214a9da0b1c654b
natha <108113193+Mandaiya@users.noreply.github.com>
2024-08-14 19:10:50 +0530

    Update logger.py

--- ArchMusic/plugins/sudo/logger.py
+++ ArchMusic/plugins/sudo/logger.py
@@ -1,9 +1,9 @@
 from pyrogram import filters
 
-from AviaxMusic import app
-from AviaxMusic.misc import SUDOERS
-from AviaxMusic.utils.database import add_off, add_on
-from AviaxMusic.utils.decorators.language import language
+from ArchMusic import app
+from ArchMusic.misc import SUDOERS
+from ArchMusic.utils.database import add_off, add_on
+from ArchMusic.utils.decorators.language import language
 
 
 @app.on_message(filters.command(["logger"]) & SUDOERS)
